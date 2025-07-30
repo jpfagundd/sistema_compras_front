ab5ecbe008460a5e62a6c50662bab835eedfebe9
João Paulo Fagundes <jpfagundcontato@gmail.com>
2025-07-29 22:30:22 -0300

    Estilização feita

--- js/produtoListar.js
+++ js/produtoListar.js
@@ -1,10 +1,40 @@
 let res = document.getElementById('res')
 
 let btn = document.getElementById('btn')
+let listarId = document.getElementById('listarId')
+
+const tabelaProdutos = document.getElementById('produtos-tabela')
 
 btn.addEventListener('click', ()=>{
     res.innerHTML = ''
     fetch(`http://localhost:3000/produto`)
+    .then(resp => resp.json())
+    .then(produto =>{
+        tabelaProdutos.innerHTML = '';
+
+        produto.forEach(produto => {
+            const linha = document.createElement('tr')
+            linha.innerHTML = `
+            <td>${produto.id}</td>
+            <td>${produto.titulo}</td>
+            <td>${produto.descricao}</td>
+            <td>${produto.categoria}</td>
+            <td>${produto.preco}</td>
+            <td>${produto.percentualDesconto}</td>
+            <td>${produto.estoque}</td>
+            <td>${produto.marca}</td>
+            `
+            tabelaProdutos.appendChild(linha)
+        })
+    })
+    .catch((err)=>{
+        console.error('Erro ao listar os produtos!',err)
+    })
+})
+
+listarId.addEventListener('click', ()=>{
+    id = document.getElementById('id')
+    fetch(`http://localhost:3000/produto/listar/:id`)
     .then(resp => resp.json())
     .then(dados =>{
         dados.forEach(dad => {
@@ -12,6 +42,6 @@ btn.addEventListener('click', ()=>{
         })
     })
     .catch((err)=>{
-        console.error('Erro ao listar os produtos!',err)
+        console.error('Erro ao listar o produto!',err)
     })
 })
--- js/usuarioListar.js
+++ js/usuarioListar.js
@@ -2,9 +2,40 @@ let res = document.getElementById('res')
 
 let btn = document.getElementById('btn')
 
+const tabelaUsuarios = document.getElementById('usuarios-tabela')
+
 btn.addEventListener('click', ()=>{
     res.innerHTML = ''
     fetch(`http://localhost:3000/usuario`)
+    .then(resp => resp.json())
+    .then(usuario =>{
+        tabelaUsuarios.innerHTML = '';
+
+        usuario.forEach(usuario => {
+            const linha = document.createElement('tr')
+            linha.innerHTML = `
+            <td>${usuario.id}</td>
+            <td>${usuario.primeiroNome}</td>
+            <td>${usuario.sobrenome}</td>
+            <td>${usuario.idade}</td>
+            <td>${usuario.email}</td>
+            <td>${usuario.telefone}</td>
+            <td>${usuario.endereco}</td>
+            <td>${usuario.cidade}</td>
+            <td>${usuario.estado}</td>
+            <td>${usuario.dataNasc}</td>
+            `
+            tabelaUsuarios.appendChild(linha)
+        })
+    })
+    .catch((err)=>{
+        console.error('Erro ao listar os usuários!',err)
+    })
+})
+
+listarId.addEventListener('click', ()=>{
+    id = document.getElementById('id')
+    fetch(`http://localhost:3000/usuario/listar/:id`)
     .then(resp => resp.json())
     .then(dados =>{
         dados.forEach(dad => {
@@ -12,6 +43,6 @@ btn.addEventListener('click', ()=>{
         })
     })
     .catch((err)=>{
-        console.error('Erro ao listar os usuários!',err)
+        console.error('Erro ao listar o usuário!',err)
     })
 })
